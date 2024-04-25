76f68496860a5796a68896318e200117a0a0d425
Gonzalo Araya Zamora <garayaz@gmail.com>
2024-04-24 20:56:02 -0400

    Cambios varios hasta el 24-04-2024

--- app/src/main/java/araya/gonzalo/alkewallwt/LoginPage.kt
+++ app/src/main/java/araya/gonzalo/alkewallwt/LoginPage.kt
@@ -3,13 +3,21 @@ package araya.gonzalo.alkewallwt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val signup = findViewById<TextView>(R.id.lp_crear_cuenta_nueva)
+        signup.setOnClickListener {
+            val abrirPantallaLogin = Intent(this, SignupPage::class.java)
+            abrirPantallaLogin.putExtra("nombre", "Gonzalo")
+            abrirPantallaLogin.putExtra("apellido", "Araya")
+            abrirPantallaLogin.putExtra("acepto_tyC", false)
+            startActivity(abrirPantallaLogin)
+        }
 
     }
 }
--- app/src/main/java/araya/gonzalo/alkewallwt/LoginSignupPageActivity.kt
+++ app/src/main/java/araya/gonzalo/alkewallwt/LoginSignupPageActivity.kt
@@ -4,21 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 
 class LoginSignupPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val signup = findViewById<Button>(R.id.button)
-        signup.setOnClickListener {
-            val abrirPantallaLogin = Intent(this, SignupPage::class.java)
-            abrirPantallaLogin.putExtra("nombre", "Gonzalo")
-            abrirPantallaLogin.putExtra("apellido", "Araya")
-            abrirPantallaLogin.putExtra("acepto_tyC", false)
-            startActivity(abrirPantallaLogin)
-        }
+
 
     }
 }
