da2c92ae3c673577fb9c4578b6c2d5deffaa21b6
Gonzalo Araya Zamora <garayaz@gmail.com>
2024-05-10 22:40:50 -0400

    configurado MVVM

--- app/src/main/java/araya/gonzalo/alkewallwt/SplashScreenActivity.kt
+++ app/src/main/java/araya/gonzalo/alkewallwt/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.lifecycle.viewmodel.viewModelFactory
 import java.util.Timer
 import java.util.TimerTask
 
